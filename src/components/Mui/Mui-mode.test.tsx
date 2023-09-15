13849920e18a9fe116efd5b7615b82e485b29034
Calvin Ucheoma <calvinchukwuma@gmail.com>
2023-09-15 13:03:22 +0100

    custom hooks test

--- src/components/Mui/Mui-mode.test.tsx
+++ src/components/Mui/Mui-mode.test.tsx
@@ -1,8 +1,11 @@
-import { screen, render } from '@testing-library/react';
+// import { screen, render } from '@testing-library/react';
+import { screen, render } from '../../test-utils';
 import { MuiMode } from './Mui-mode';
+// import { AppProviders } from '../Providers/AppProviders';
 
 describe('MuiMode', () => {
   test('renders text correctly', () => {
+    // render(<MuiMode />, { wrapper: AppProviders });
     render(<MuiMode />);
     const headerElement = screen.getByRole('heading');
     expect(headerElement).toHaveTextContent('dark mode');
